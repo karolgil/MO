20f3b4445691fd42bc85db15c12b04e92c29ff91
karolgil <karol.gil@wp.pl>
2014-09-16 22:17:14 +0200

    Minor change

--- generuj_rozklad.py
+++ generuj_rozklad.py
@@ -1,12 +1,18 @@
 from itertools import permutations
+from random import randint
 
 def generuj_rozklad():
 	with open("miasta.data", 'r') as handler_do_pliku:
 		miasta = []
 		polaczenia = []
 		for linia in handler_do_pliku:
-			miasta.append(str(linia))
-			polaczenia = permutations(miasta)
-			data = list(map("".join, itertools.permutations(miasta)))
-			print(data)
+			miasta.append(str(linia.split()[0]))
+		polaczenia = permutations(miasta)
+		permutacje = list(map(" ".join, permutations(miasta, 2)))
+		with open("rozklady_jazdy.data", 'w') as handler_do_pliku:
+			for trasa in permutacje:
+				for _ in range(randint(1,4)):
+					linia_do_pliku = " ".join([str(trasa),str(randint(1,23)),str(randint(1,5))])
+					handler_do_pliku.write(linia_do_pliku + "\n")
+		
 generuj_rozklad()
